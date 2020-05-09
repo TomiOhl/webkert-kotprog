1a7e4dc5a781c7713dc0478e82351459093d6f51
Öhlsläger Tomáš <h881446@stud.u-szeged.hu>
2020-05-09 17:28:01 +0200

    Mockolt tartalom frissitese

--- src/app/hot/hotlist.ts
+++ src/app/hot/hotlist.ts
@@ -2,27 +2,70 @@ import { HotItem } from './hotitem';
 
 export const HOTLIST: HotItem[] = [
     {
-        title: 'Avengers: Endgame',
-        subtitle: 'Thanos',
-        img: 'https://images5.alphacoders.com/998/thumb-1920-998470.jpg',
-        content: 'false',
+        title: 'Egyre több embernek kellenének most jó hírek',
+        subtitle: `Úgy tűnik, hogy kezdik egyre nehezebben viselni a járvány hatásait az emberek:
+        soha nem kerestek még ennyien a "jó hírek" kifejezésre.`,
+        img: 'https://i.cdn29.hu/apix_collect_c/2001/koronavirus/koronavirus_screenshot_20200403122318_2_original_760x760.webp',
+        content: `A Google Trends adatbázisa szerint soha nem keresték még ennyien a Google keresőjével a "jó hírek" kifejezésre.
+        A Google 2004 óta teszi közzé a keresési adatokat, és azóta a valaha mért legmagasabb keresési számra is
+        rádupláztak most az emberek a "jó hírek" szókapcsolat esetében. Bár konkrét számokat nem közöltek, az kiderült,
+        hogy pár hónapja még csak 25 százaléka volt a keresések száma a mainak, erre a konkrét kifejezésre.
+        Az Ahrefs.com adatai alapján a jó hírek szavakra való keresések száma tavaly decemberben nagyjából 78 000 volt átlagosan,
+        így most a havi mennyiség 500 000 körül lehet.
+        Talán mondani sem kell, de a koronavírus szóra sem keresték még soha ennyiszer az emberek, nem volt nehéz 100%-os
+        növekedést elérni ezzel a kulcsszóval az elmúlt hónapok során. Több szakértő felhívta a figyelmet,
+        hogy ezekben az időkben különösen fontos a pozitív hozzáállás, és, hogy nekünk tetsző, pozitív hangvételű témákkal,
+        tartalmakkal is foglalkozzunk, amelyek kiszakítanak a járvány és annak gazdasági hatásai által keltett pánikhangulatból.
+        (forrás: pcworld.hu)`,
     },
     {
-        title: 'Spiderman',
-        subtitle: 'Pokcsávó',
-        img: 'https://am23.mediaite.com/tms/cnt/uploads/2019/09/Ned-Was-Originally-in-the-Mid-Credit-Scenes-for-Far-from-Home-1200x675.jpg',
-        content: 'false',
+        title: 'Pénteken egyetlen új fertőzöttet sem találtak Szlovákiában',
+        subtitle: '3910 tesztet végeztek pénteken Szlovákiában és egyetlen pozitív sem akad köztük.',
+        // tslint:disable-next-line: max-line-length
+        img: 'https://ujszo.com/sites/default/files/styles/pl_article_full_lead/public/lead_image/202603vlada_laboratoria_covid19_3692847.jpg?h=e4ca357b',
+        content: `Aznap ráadásul egy ember sem halt meg a betegségben, miközben 14-en meggyógyultak.
+        Az országban összesen négy beteg van intenzív osztályon és egyetlen embert kell lélegeztetőgépen tartani.
+        Nemhiába nyilatkozta azt Marek Krajči egészségügyi miniszter, hogy
+        “A járványellenes intézkedésekkel szinte teljesen sikerül eliminálni a koronavírust Szlovákiában.”
+        Azt is hozzátette azért, hogy:
+        “a Covid-19 elleni háborút még korántsem nyertük meg, de ha az emberek fegyelmezettek lesznek, sikeresek lehetünk.”
+        (forrás: ujszo.com, 444.hu)`,
     },
     {
-        title: 'Matrix',
-        subtitle: 'Neo és az ügynökök',
-        img: 'https://img.huffingtonpost.com/asset/5cfe99db240000120f85a95f.jpeg?cache=idtumdl0az&ops=1778_1000',
-        content: 'false',
+        title: 'Büntetés vagy lehetőség a nyári iskola?',
+        subtitle: `A szakemberek nem lelkesednek a kormányfő által beharangozott nyári iskoláért.
+        Bár számos tanulónak nincs internet-hozzáférése, a szakértők úgy vélik, akik akartak,
+        be tudtak kapcsolódni az oktatásba anélkül is.`,
+        img: 'https://zskrupina.sk/wp-content/uploads/2019/11/hodina-detom_004.jpg',
+        content: `Az Oktatáspolitikai Intézet elemzése szerint 23 000 és 32 000 közé tehető azon hazai alapiskolások száma,
+        akiknek nincs internet-hozzáférésük. Legtöbbjük a Besztercebányai, az Eperjesi és a Kassai kerületben él.
+        Számukra a március közepe óta folyó internetes távoktatás elképzelhetetlen.
+        A három megyében a becslések szerint körülbelül négyezer negyedikesnek nincs otthon internetje.
+        Elsősorban a szociálisan hátrányos helyzetben levő – nem kizárólag roma – gyerekek nem tudnak csatlakozni a világhálóhoz.
+        Egy 2019 áprilisában végzett reprezentatív felmérés szerint a szegényebb háztartásokban élő gyerekek 52%-a
+        tud otthon internetezni, roma háztartásokban ez az arány 40%. Ezzel szemben az átlagos körülmények közt élő gyerekek 95%-ának
+        van otthon internet-hozzáférése.\nA szakemberek az elemzésükben arra is figyelmeztettek,
+        ehhez társul az is, hogy a hátrányos helyzetben levő gyerekek többségének nincsenek megfelelő technikai eszközeik,
+        valamint a számítógépes kompetenciáik is gyengébbek, mint az átlagos családban élő tanulóké.\n
+        Igor Matovič (OĽaNO) miniszterelnök a problémát nyári iskola megnyitásával oldaná meg.
+        „Gyermekek tízezreinek nincs otthon internet-hozzáférése, számítógépe vagy táblagépe.
+        Ha véget ér a »szünet« és az azt követő nyári vakáció, óriási hátrányban lesznek a kortársaikhoz képest”
+        – írta a kormányfő a közösségi oldalán. „Helyes ezeknek a gyerekeknek lehetőséget adni,
+        hogy nyáron egy hónapig az iskolában behozhassák a lemaradást?” – tette fel a kérdést Matovič.
+        A nyári szünet alatti tanításról már Branislav Gröhling (SaS) oktatási miniszter is beszélt.
+        Elmondta, önkéntes alapon működne, és nem a megszokott módszerekkel folyna az oktatás.
+        (forrás: ujszo.com)`,
     },
     {
-        title: 'Én kicsi pónim',
-        subtitle: 'Fabolous',
-        img: 'https://wallpapercave.com/wp/m856jFo.jpg',
-        content: 'false',
+        title: 'Itt az első villanyautó, amelynek akkumulátorára tíz év garanciát adnak',
+        subtitle: 'A Lexus első elektromos autója, az UX 300e jövőre érkezik. Lesz egy nagy újdonsága.',
+        img: 'https://cdn.nwmgroups.hu/s/img/i/2005/20200508ux-300e.jpg?w=644&h=430',
+        content: `A jelenleg 95 százalékban öntöltő hibrid elektromos hajtással gazdára találó UX
+        kompakt crossover testvérmodelljéhez egyedülálló feltételeket kínál a gyártó:
+        tízéves vagy egymillió kilométeres akkumulátor-garancia jár majd hozzá.
+        Az UX 300e energiaellátásáról új fejlesztésű, nagy kapacitású (54,3 kWh) akkumulátorcsomag gondoskodik,
+        amely a WLTP norma szerint több mint 300 kilométeres hatótávolságot tesz lehetővé.
+        A 288 cellából álló lítium-ion akkumulátorcsomag az utastér padlója, illetve a hátsó ülések alatt helyezkedik el.
+        (forrás: origo.hu)`,
     }
 ];
--- src/app/tech/techlist.ts
+++ src/app/tech/techlist.ts
@@ -11,6 +11,7 @@ export const TECHLIST: Techitem[] = [
         title: 'Második',
         // tslint:disable-next-line: max-line-length
         img: 'https://www.wikihow.com/images/thumb/8/87/Make-Your-Guinea-Pig-Happy-Without-a-Second-Guinea-Pig-Step-4-Version-3.jpg/aid1354661-v4-728px-Make-Your-Guinea-Pig-Happy-Without-a-Second-Guinea-Pig-Step-4-Version-3.jpg.webp',
-        subtitle: 'Na ezt ne most'
+        subtitle: 'Na ezt ne most',
+        content: 'Lehet kellenek majd hosszabb cikkek tho'
     }
 ];
